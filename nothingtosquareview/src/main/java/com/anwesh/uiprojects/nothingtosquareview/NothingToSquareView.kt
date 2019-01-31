cf3c7761435f1f549076deb858323ed9e6abe58a
Anwesh43 <anweshthecool0@gmail.com>
2019-01-31 10:52:10 +0530

    adding NothingToSquare class

--- nothingtosquareview/src/main/java/com/anwesh/uiprojects/nothingtosquareview/NothingToSquareView.kt
+++ nothingtosquareview/src/main/java/com/anwesh/uiprojects/nothingtosquareview/NothingToSquareView.kt
@@ -178,7 +178,30 @@ class NothingToSquareView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class NothingToSquare(var i : Int) {
+
+        private var curr : NTSNode = NTSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
