289fbe30cbb6456f19bd4332807dfb56cab0771c
Anwesh43 <anweshthecool0@gmail.com>
2019-01-31 10:25:36 +0530

    addint fdrawNTSNode method

--- nothingtosquareview/src/main/java/com/anwesh/uiprojects/nothingtosquareview/NothingToSquareView.kt
+++ nothingtosquareview/src/main/java/com/anwesh/uiprojects/nothingtosquareview/NothingToSquareView.kt
@@ -28,3 +28,47 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawRotateLine(x : Float, y: Float, size : Float, scale : Float, paint : Paint) {
+    save()
+    translate(x, y)
+    rotate(-90f * (1 - scale.divideScale(1, 2)))
+    drawLine(0f, 0f, size * scale.divideScale(0, 2), 0f, paint)
+    restore()
+}
+
+fun Canvas.drawVerticalLines(x : Float, y: Float, paint : Paint) {
+    for (j in (0..steps - 1)) {
+        val sx : Float = x * (1f - 2 * j)
+        drawLine(sx, 0f, sx, y, paint)
+    }
+}
+
+
+fun Canvas.drawNTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val xGap = (2 * size) / lines
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w/2, gap * (i + 1))
+    for (j in (0..(steps - 1))) {
+        val sc : Float = sc2.divideScale(j, 2)
+        val y : Float = size * (1 - 2 * j) * sc
+        drawVerticalLines(-size, y, paint)
+        save()
+        translate(-size, y)
+        for (k in (0..(lines - 1))) {
+            val sck : Float = sc1.divideScale(k, lines)
+            drawRotateLine(k * xGap, y, xGap, sck, paint)
+        }
+        restore()
+    }
+    restore()
+}
