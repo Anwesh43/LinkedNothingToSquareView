8f02ce2442c16eaeb81b922788f2e074b9a60c19
Anwesh43 <anweshthecool0@gmail.com>
2019-01-31 10:05:24 +0530

    importing libraries and defining constants

--- nothingtosquareview/src/main/java/com/anwesh/uiprojects/nothingtosquareview/NothingToSquareView.kt
+++ nothingtosquareview/src/main/java/com/anwesh/uiprojects/nothingtosquareview/NothingToSquareView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.nothingtosquareview
 /**
  * Created by anweshmishra on 31/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val steps : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#00C853")
+val backColor : Int = Color.parseColor("#BDBDBD")
